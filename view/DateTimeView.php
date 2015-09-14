b1bb77b4267668009309bdfe76559f2d6bb670cd
Hampus <hj222hi@student.lnu.se>
2015-09-14 13:35:40 +0200

    11%
    Vi have a start :)

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -4,8 +4,9 @@ class DateTimeView {
 
 
 	public function show() {
-
-		$timeString = 'TODO, Write servertime here...';
+		
+		$timeString = date('l').', the '.date('d').'th of '.date('F Y').', The time is ' .date('h:i:s', time());
+		 //= 'TODO, Write servertime here...';
 
 		return '<p>' . $timeString . '</p>';
 	}
